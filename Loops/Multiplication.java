8e2d880ccaaa0578d7ea7aafdfa0b1cbf3c97f3c
Nipanepiyush <piyushnipane74@gmail.com>
2026-07-16 13:31:55 +0530

    Basic Loops Program

---
+++ Loops/Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Multiplication {
+       public static void main(String[] args) {
+              Scanner scan = new Scanner(System.in);
+              System.out.print("Enter No:");
+              int a = scan.nextInt();
+
+              for (int i = 1; i <= 10; i++) {
+                     System.out.println(a * i);
+              }
+
+       }
+}
--- Loops/SumOfNNumbers.java
+++ Loops/SumOfNNumbers.java
@@ -1,7 +1,20 @@
+import java.util.*;
+
 public class SumOfNNumbers {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
 
-        
-     }
+        int N = scan.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= N; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println("Sum = " + sum);
+
+        scan.close();
+    }
 }
